68fb55a952549cc3f339e2007c33c976d5b76421
Jeremy Bruzac <bruzacj@yahoo.fr>
2022-03-11 10:17:07 +0100

    Sequelize init and migration db

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,1 +1,27 @@
-const sequelize = require('sequelize');
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    static associate(models) {
+      // define association here
+      models.Post.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      })
+    }
+  }
+  Post.init({
+    userId: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    likes: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Post',
+  });
+  return Post;
+};
---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+      models.User.hasMany(models.Post)
+    }
+  }
+  User.init({
+    email: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    bio: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
