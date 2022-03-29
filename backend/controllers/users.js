33cb5fa74baedfe193a95e4ec5f9a806edce619d
Jeremy Bruzac <bruzacj@yahoo.fr>
2022-03-29 15:36:46 +0200

    Manage and check token user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -11,50 +11,73 @@ dotEnv.config();
 
 
 exports.signup = (req, res, next) => {
-   console.log(req.body);
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = models.User.build({
-                email: req.body.email,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                password: hash,
-                isAdmin: false,
-                
-            });
-            if (user.email == null || user.firstName == null || user.lastName == null || user.password == null) {
-              return res.status(400).json({ 'error': 'Champs manquants !'})
-            } else {
-              user.save()
-              .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
-              .catch(error => res.status(400).json({ error }));
-            }  
-        })
-        .catch(error => res.status(500).json({ error }));
+
+    if (req.body.email == null || req.body.firstName == null || req.body.lastName == null || req.body.password == null) {
+      return res.status(400).json({ error: 'Champs manquants !'})
+    }
+    // Check if unique email in Database
+    models.User.findOne({
+      attributes: ['email'],
+      where: { email: req.body.email}
+    })
+      .then((userFound) => {
+      // Create user if he doesn't exist
+        if(!userFound) {
+          bcrypt.hash(req.body.password, 10, function(err, hash) {
+            const user = models.User.create({
+              email: req.body.email,
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              password: hash,
+              isAdmin: false,
+            })
+              .then((user) => {
+                res.status(201).json({
+                  userId: user.id,
+                  token: jwt.sign(
+                    { userId: user.id },
+                    process.env.DB_TOKEN_SECRET,
+                    { expiresIn: '24h'},
+                  ),
+                  Message: 'Utilisateur créé'
+                });
+              })
+              .catch(err => res.status(500).json({ error }));
+          }) 
+        } else {
+          return res.status(409).json({ error: 'Cet mail est déjà utilisé !' });
+        } 
+      })
+      .catch(() => res.status(500).json({ error: "Incapable de vérifier l'utilisateur" })); 
 };
 
 exports.login = (req, res, next) => {
+  //Check if values == empty
+  if (req.body.email == null || req.body.password == null) {
+    return res.status(400).json({ 'error': 'Champs manquants !'})
+  }
   models.User.findOne({ where: { email: req.body.email } })
-    .then(user => {
-      console.log(user);
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !'});
-      }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-            if (!valid) {
-                return res.status(401).json({ error: "Mot de passe incorrect !"})
-            }
-            res.status(200).json({
-                user: user,
-                token: jwt.sign(
-                  { userId: user.id },
-                  process.env.DB_TOKEN_SECRET,
-                  { expiresIn: '24h'}
-                )
+
+    .then(userFound => {
+      if (userFound) {
+        bcrypt.compare(req.body.password, userFound.password, function( err, resBcrypt) {
+          if(resBcrypt) {
+            return res.status(200).json({
+              userId: userFound.id,
+              token: jwt.sign(
+                { userId: userFound.id },
+                process.env.DB_TOKEN_SECRET,
+                { expiresIn: '24h'},
+              ),
+              Message: 'Vous êtes bien connecté ' + userFound.firstName + ' ' + userFound.lastName
             });
-        })
-        .catch(error => res.status(500).json({ error }));
+          } else {
+            return res.status(403).json({ error: "Mot de passe incorrect !" });
+          }
+        })  
+      } else {
+        return res.status(404).json({ error: 'Utilisateur non trouvé !'});
+      }  
     })
-    .catch(error => res.status(500).json({ error }));
+    .catch(() => res.status(500).json({ error: "Incapable de vérifier l'utilisateur" }))
 };
